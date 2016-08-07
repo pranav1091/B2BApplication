4c8cbd968741999d5d73a4ebc82f8b020e3c0da9
pranav1091 <pranav1091@gmail.com>
2016-08-06 17:58:42 -0700

    b2b v2.2

---
+++ eCommerce.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using eCommerce.Contracts.Repositories;
+using eCommerce.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eCommerce.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        IRepositoryBase<Customer> customers;
+        IRepositoryBase<Product> products;
+
+        public AdminController(IRepositoryBase<Customer> customers, IRepositoryBase<Product> products)
+        {
+            this.customers = customers;
+            this.products = products;
+        }
+
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ eCommerce.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using eCommerce.Contracts.Repositories;
+using eCommerce.DAL.Data;
+using eCommerce.DAL.Repositories;
+using eCommerce.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eCommerce.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        IRepositoryBase<Customer> customers;
+        IRepositoryBase<Product> products;
+
+        public HomeController(IRepositoryBase<Customer> customers, IRepositoryBase<Product> products)
+        {
+            this.customers = customers;
+            this.products = products;
+        }
+
+        public ActionResult Index()
+        {
+            //CustomerRepository customers = new CustomerRepository(new DataContext());
+            ProductRepository products = new ProductRepository(new DataContext());
+
+           //  = new CustomerRepository(new DataContext());
+         
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
